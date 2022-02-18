9ddb6caa09d1c0ba0908c57bad972c87ee790a29
Jakob Voss <voss@gbv.de>
2022-02-18 14:07:06 +0100

    Remove parser key in format response

--- routes/formats.js
+++ routes/formats.js
@@ -1,4 +1,5 @@
 export default function (req, res) {
   const service = req.app.get("validationService")
-  res.json(service.listFormats(req.query))
+  // eslint-disable-next-line no-unused-vars
+  res.json(service.listFormats(req.query).map(({parser, ...f}) => f))
 }
