b39e73e2a06d7b2d666c54a3f9d977130a2a68be
Jakob Voss <voss@gbv.de>
2022-03-08 13:25:25 +0100

    Merge config code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import createService from "./lib/service.js"
-import loadConfig from "./lib/config.js"
-import validateConfig from "./lib/validate-config.js"
+import { loadConfig, validateConfig } from "./lib/config.js"
 import knownFormats from "./lib/formats.js"
 
 export { loadConfig, validateConfig, createService, knownFormats }
