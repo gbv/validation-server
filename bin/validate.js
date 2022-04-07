cf4b3ef20dc7e328b30f86d097759183cce8cc9a
Jakob Voss <voss@gbv.de>
2022-04-07 11:24:51 +0200

    Improve performance

--- bin/validate.js
+++ bin/validate.js
@@ -4,7 +4,7 @@ import validate from "../lib/cli.js"
 
 validate(process.argv.slice(2), console.log)
   .then(process.exit)
-  .catch(e => { 
+  .catch(e => {
     console.error(e.message)
     process.exit(2)
   })
